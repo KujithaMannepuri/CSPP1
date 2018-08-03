fee08aa59b778a391fadf23288d1c2f2f794a8c7
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-03 18:39:51 +0530

    " -> 3 of 3 passed. pylint: 0.00/10 "

--- module 5/p2/square_root.py
+++ module 5/p2/square_root.py
@@ -1,4 +1,5 @@
 # Write a python program to find the square root of the given number
+'''square root exception'''
 # using approximation method
 
 # testcase 1
@@ -10,19 +11,20 @@
 # output: 6.999999999999991
 
 def main():
+	'''square root exception'''
 	square = int(input())
 	# epsilon and step are initialized
 	# don't change these values
 	epsilon = 0.01
 	step = 0.1
-	guess=0.0
+	guess = 0.0
 	# your code starts here
-	while abs(guess**2 - square)>=epsilon:
+	while abs(guess**2 - square) >= epsilon:
 		guess += step
-	if abs(guess**2-square)>=epsilon:
-		print('failed', square)
+	if abs(guess**2-square) >= epsilon:
+		print()
 	else:
 		print(guess)
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
