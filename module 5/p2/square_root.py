72b996ea6afc69a37c7ba79b05cd2e58292f1eb7
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-03 18:40:24 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- module 5/p2/square_root.py
+++ module 5/p2/square_root.py
@@ -11,20 +11,20 @@
 # output: 6.999999999999991
 
 def main():
-	'''square root exception'''
-	square = int(input())
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	guess = 0.0
-	# your code starts here
-	while abs(guess**2 - square) >= epsilon:
-		guess += step
-	if abs(guess**2-square) >= epsilon:
-		print()
-	else:
-		print(guess)
+    '''square root exception'''
+    square = int(input())
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    step = 0.1
+    guess = 0.0
+    # your code starts here
+    while abs(guess**2 - square) >= epsilon:
+        guess += step
+    if abs(guess**2-square) >= epsilon:
+        print()
+    else:
+        print(guess)
 
 if __name__ == "__main__":
-	main()
+    main()
