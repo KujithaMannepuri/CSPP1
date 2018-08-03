c7c0a82c8d0188e1e88d0c06291488a235b04afb
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-03 17:12:30 +0530

    " -> 3 of 3 passed. pylint: -5.56/10 "

--- module 5/p4/square_root_newtonrapson.py
+++ module 5/p4/square_root_newtonrapson.py
@@ -1,9 +1,22 @@
-# Write a python program to find the square root of the number 
-# using Newton-Rapson method
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
 
 def main():
-	s = raw_input()
+	num = int(input())
 	#your code here
+	epsilon=0.01
+	root=num/2.0
+	while abs(root*root-num)>=epsilon:
+		root=root-(((root**2)-num)/(2*root))
+	print(str(root))
 
 if __name__== "__main__":
 	main()
