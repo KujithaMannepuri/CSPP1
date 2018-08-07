8c877a6c46006708b5578553917b11add4ec84cd
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-07 15:09:33 +0530

    " -> 9 of 10 passed. pylint: 9.09/10 "

--- module 8/power using Iteration/power_iter.py
+++ module 8/power using Iteration/power_iter.py
@@ -1,32 +1,26 @@
+'''power iteration'''
 # Exercise: PowerIter
-# Write a Python function, iterPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
+# Write a Python function, iterPower(base, exp), that takes in two
+#numbers and returns the base^(exp) of given base and exp.
 
 # This function takes in two numbers and returns one number.
 
 
-def iterPower(base, exp):
+def iterpower(base, exp):
     '''
     base: int or float.
     exp: int >= 0
- 
     returns: int or float, base^exp
     '''
-    result=1
-    for i in range (exp):
-         
-        result = result*base
-            
-      
+    result = 1
+    for _ in range(exp):
+        result = result * base
     return result
-
-    
-
-
 def main():
-
+    '''power iteration'''
     data = input()
     data = data.split()
-    print(iterPower(float(data[0]),int(data[1]))) 
+    print(iterpower(float(data[0]), int(data[1])))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
