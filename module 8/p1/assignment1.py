22523637700fa2d2e07d5df861fa28bc7fdc0968
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-07 12:54:07 +0530

    " -> 8 of 10 passed. pylint: 10.00/10 "

---
+++ module 8/p1/assignment1.py
@@ -0,0 +1,23 @@
+'''factorial of a number'''
+# Exercise: Assignment-1
+# Write a Python function, factorial(n), that takes in one
+#number and returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+def fact(num):
+    '''
+    n is positive Integer
+
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+    if num in (1, 0):
+        return 1
+    return num * fact(num - 1)
+def main():
+    ''' factorial of a number'''
+    num = input()
+    print(fact(int(num)))
+if __name__ == "__main__":
+    main()
