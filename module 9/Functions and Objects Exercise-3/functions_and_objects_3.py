c0a4ebca2c356bac192a16a7671631ec7a320ab8
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-08 19:16:48 +0530

    functions and objects 3

--- module 9/Functions and Objects Exercise-3/functions_and_objects_3.py
+++ module 9/Functions and Objects Exercise-3/functions_and_objects_3.py
@@ -1,19 +1,20 @@
+''' functions and objects 3'''
 #Exercise : Function and Objects Exercise-3
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
-
-def square(L):
-	return L*L
-def apply_to_each(L, f):
-	return list(map(square, L))
-    
-    
+def square(lst):
+    ''' functions and objects 3'''
+    return lst*lst
+def apply_to_each(lst, _):
+    ''' functions and objects 3'''
+    return list(map(square, lst))
 def main():
-	data = input()
-	data = data.split()
-	list1 = []
-	for j in data:
-		list1.append(int(j))
-	print(apply_to_each(list1, square))
+    ''' functions and objects 3'''
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    print(apply_to_each(list1, square))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
