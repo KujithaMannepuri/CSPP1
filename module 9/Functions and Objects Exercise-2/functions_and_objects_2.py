7981aad8933cb2075d3d24ec29ecf801e50f5041
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-08 19:12:16 +0530

    functions and objects 2

--- module 9/Functions and Objects Exercise-2/functions_and_objects_2.py
+++ module 9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -1,13 +1,15 @@
+''' functions and objects 2'''
 #Exercise : Function and Objects Exercise-2
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [2, -3, 9, -8]
 
-def inc(L):
-	return L+1
-def apply_to_each(L, f):
-    
-	return list(map(inc, L))
-
+def inc(lst):
+    ''' functions and objects 2'''
+    return lst+1
+def apply_to_each(lst, _):
+    ''' functions and objects 2'''
+    return list(map(inc, lst))
 def main():
+    ''' functions and objects 2'''
     data = input()
     data = data.split()
     list1 = []
