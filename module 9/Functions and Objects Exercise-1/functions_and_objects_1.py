bfd3b00906092b74b1d551d7ad66a06355d4219f
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-08 19:06:10 +0530

    functions and objects

--- module 9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ module 9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -1,17 +1,20 @@
+'''test list'''
 #Exercise : Function and Objects Exercise-1
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
 
 
-def apply_to_each(L, f):
-    return list(map(abs, L))
+def apply_to_each(lst, _):
+    '''test list'''
+    return list(map(abs, lst))
 
 def main():
+    '''test list'''
     data = input()
     data = data.split()
     list1 = []
     for j in data:
         list1.append(int(j))
-    print (apply_to_each(list1, abs))
+    print(apply_to_each(list1, abs))
 
 if __name__ == "__main__":
     main()
