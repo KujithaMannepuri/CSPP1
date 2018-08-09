aaa6a5d2fcece495cd0f55d6e09d9f3d9e32a742
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-09 21:12:18 +0530

    biggest_exercise

--- module 10/biggest Exercise/biggest_exercise.py
+++ module 10/biggest Exercise/biggest_exercise.py
@@ -1,8 +1,11 @@
+'''biggest exercise'''
 #Exercise : Biggest Exercise
-#Write a procedure, called biggest, which returns the key corresponding to the entry with the largest number of values associated with it. If there is more than one such entry, return any one of the matching keys.
+#Write a procedure, called biggest, which returns the key corresponding to
+# the entry with the largest number of values associated with it.
+# If there is more than one such entry, return any one of the matching keys.
 
 
-def biggest(aDict):
+def biggest(adict):
     '''
     aDict: A dictionary, where all the values are lists.
 
@@ -11,24 +14,26 @@ def biggest(aDict):
     # Your Code Here
     maxm = 0
     best = ""
-    for index in aDict:
-        if len(aDict[index])>maxm:
-            maxm = len(aDict[index])
+    for index in adict:
+        if len(adict[index]) > maxm:
+            maxm = len(adict[index])
             best += index
     return best[-1]
 
 def main():
-    n=input()
-    aDict={}
-    for i in range(int(n)):
-        s=input()
-        l=s.split()
-        if l[0][0] not in aDict:
-            aDict[l[0][0]]=[l[1]]
+    '''biggest exercise'''
+    num = input()
+    adict = {}
+    for _ in range(int(num)):
+        string = input()
+        lgth = string.split()
+        if lgth[0][0] not in adict:
+            adict[lgth[0][0]] = [lgth[1]]
         else:
-            aDict[l[0][0]].append(l[1])
-    print(biggest(aDict))
+            adict[lgth[0][0]].append(lgth[1])
+    print(biggest(adict))
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
+    
