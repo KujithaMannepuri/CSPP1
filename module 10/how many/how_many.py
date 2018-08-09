52970e82d346cde60de78d1043ecb8b9eecd0e5d
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-09 21:05:53 +0530

    how_many

--- module 10/how many/how_many.py
+++ module 10/how many/how_many.py
@@ -1,31 +1,34 @@
+'''how many'''
 #Exercise : how many
-# write a procedure, called how_many, which returns the sum of the number of values associated with a dictionary.
+# write a procedure, called how_many, which returns the sum of the
+# number of values associated with a dictionary.
 
 
-def how_many(aDict):
+def how_many(adict):
     '''
     aDict: A dictionary, where all the values are lists.
 
     returns: int, how many values are in the dictionary.
     '''
     # Your Code Here
-    sum=0
-    value=aDict.values()
+    sum1 = 0
+    value = adict.values()
     for sublist in value:
-    	sum=sum+len(sublist)
-    return sum
+        sum1 = sum1 + len(sublist)
+    return sum1
 
 def main():
-	
-	aDict={}
-	for i in range(int(len(aDict))):
-		s=input()
-		l=s.split()
-		if l[0][0] not in aDict:
-			aDict[l[0][0]]=[l[1]]
-		else:
-			aDict[l[0][0]].append(l[1])
-	print(how_many(aDict))
+    '''how many'''
+    num = input()
+    adict = {}
+    for _ in range(int(num)):
+        string = input()
+        length = string.split()
+        if length[0][0] not in adict:
+            adict[length[0][0]] = [length[1]]
+        else:
+            adict[length[0][0]].append(length[1])
+    print(how_many(adict))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
