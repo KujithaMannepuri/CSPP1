af5178c22613a82f9909cd384496e3c788f67f53
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-11 16:10:38 +0530

    " -> 8 of 10 passed. pylint: 10.00/10 "

--- module 7/Functions - Assignment-3/assignment3.py
+++ module 7/Functions - Assignment-3/assignment3.py
@@ -1,3 +1,4 @@
+'''paying debt'''
 # Functions - Assignment-3 - Using Bisection Search to Make the Program Faster
 
 # You'll notice that in Problem 2, your monthly payment had to be a multiple of $10.
@@ -8,7 +9,7 @@
 # code runs more slowly, especially in cases with very large balances
 # and interest rates. (Note: when your code is running on our servers,
 # there are limits on the amount of computing time each submission
-# is allowed, so your observations from running this experiment on 
+# is allowed, so your observations from running this experiment on
 #the grading system might be limited to an error message complaining
 # about too much time taken.)
 
@@ -54,36 +55,34 @@
 
 
 def payingdebtoffinayear(balance, annualinterestrate):
-	'''debt off'''
-	monthlyinterest=(annualinterestrate)/12.0
-	monthlypaymentlow=balance/12
-	monthlypaymenthigh=(balance*(1+monthlyinterest)**12)/12.0
-	newbal=balance
-	epsilon=0.0001
-	amount=(monthlypaymentlow+monthlypaymenthigh)/2
-	while True:
-		month=1
-		while month<=12:
-			newbal=newbal-amount
-			newbal=newbal+(monthlyinterest*newbal)
-			month+=1
-		if newbal>0 and newbal>epsilon:
-			monthlypaymentlow=amount
-			newbal=balance
-		elif newbal<0 and newbal<epsilon:
-			monthlypaymenthigh=amountnewbal=balance
-		else:
-			amount=(monthlypaymentlow+monthlypaymenthigh)/2
-	return amount
-
-
-
+    '''debt off'''
+    monthlyinterest = (annualinterestrate) / 12.0
+    monthlypaymentlow = balance / 12
+    monthlypaymenthigh = (balance * (1 + monthlyinterest) ** 12) / 12.0
+    newbal = balance
+    epsilon = 0.0001
+    amount = (monthlypaymentlow + monthlypaymenthigh) / 2
+    while True:
+        month = 1
+        while month <= 12:
+            newbal = newbal - amount
+            newbal = newbal + (monthlyinterest * newbal)
+            month += 1
+        if newbal > 0 and newbal > epsilon:
+            monthlypaymentlow = amount
+            newbal = balance
+        elif newbal < 0 and newbal < - epsilon:
+            monthlypaymenthigh = amount
+            newbal = balance
+        else:
+            return amount
+        amount = (monthlypaymentlow + monthlypaymenthigh) / 2
 def main():
+    '''paying debt off'''
     data = input()
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment:", payingdebtoffinayear(data[0],data[1]))
-    
+    print("Lowest Payment: "+ str(round(payingdebtoffinayear(data[0], data[1]), 2)))
 if __name__ == "__main__":
     main()
