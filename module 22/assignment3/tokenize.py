3143e9a4f2e2fe80844c0c23bdbd95b44cefd6c0
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-25 13:12:54 +0530

    " -> 3 of 4 passed. pylint: 9.38/10 "

--- module 22/assignment3/tokenize.py
+++ module 22/assignment3/tokenize.py
@@ -2,12 +2,22 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
 def tokenize(string):
-    pass
-            
+    '''tokenizing a string'''
+    token = {}
+    for key in string:
+        token[key] = token.get(key, 0) + 1
+    return token   
 def main():
-    pass
+    '''main function'''
+    string = ''
+    ip_lines = int(input())
+    for index in range(ip_lines):
+        index += 1
+        string += input()
+        string += '\n'
+    string = string.split()
+    print(tokenize(string))
 
 if __name__ == '__main__':
     main()
---
+++ module 22/assignment4/print_dictionary.py
@@ -0,0 +1,19 @@
+'''
+Write a function to print a dictionary with the keys in sorted order along with the
+frequency of each word. Format of the printing should be one key per line and separate
+the key and frequency with a SPACE - SPACE.
+'''
+
+def print_dictionary(dictionary):
+    dict1 = []
+    dict['dictionary'].items()[0][0]
+    print(i,j)
+        # dict1 = dict1.append(dictionary)
+    return dict1
+
+def main():
+    dictionary = eval(input())
+    print(print_dictionary(dictionary))
+
+if __name__ == '__main__':
+    main()
